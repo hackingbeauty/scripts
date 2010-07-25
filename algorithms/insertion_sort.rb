31bbc090f308e98e9588cac4c7baf32c696008d9
Mark Muskardin <markmuskardin@mark-muskardins-macbook-pro.local>
2010-07-24 19:11:01 -0700

    Refactored

--- algorithms/insertion_sort.rb
+++ algorithms/insertion_sort.rb
@@ -3,9 +3,9 @@ class InsertionSort
   def sort(num)
     puts num.length-1
     for j in 1..(num.length - 1)
-      key = num[j] #12
-      i = j - 1 #i = 3
-      while i >= 0 and num[i] > key #
+      key = num[j]
+      i = j - 1
+      while i >= 0 and num[i] > key 
               num[i+1] = num[i]
               i = i - 1
       end
