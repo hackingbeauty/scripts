f1fae7ad18eee422dfd8093bc5d3e553510a57c0
Mark Muskardin <markmuskardin@gmail.com>
2011-04-25 19:29:55 -0700

    added divide.rb

---
+++ divide.rb
@@ -0,0 +1,26 @@
+class Divide
+	def initialize(x,y)
+		divide(x,y)
+	end
+
+	def divide(x,y)
+    val = x;
+    count = 0;
+    while true do
+      val = val - y
+      count += 1
+      if(val < y)
+        valType = false
+        puts "Quotient: #{count}, Remainder: #{val}"
+        return
+      elsif(val == 0)
+        valType = false
+        puts "Quotient: #{count}, Remainder: 0"
+        return
+      end
+    end
+	end
+
+end
+
+Divide.new(ARGV[0].to_i, ARGV[1].to_i)
